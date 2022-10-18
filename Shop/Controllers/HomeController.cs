2007ceaea7bac506a7559d854b52122ffeb0beab
sashakrlov@gmail.com <sashakrlov@gmail.com>
2022-10-18 23:24:48 +0300

    Добаивл в бд иголки и камни, создал каталог с камнями и иголками, создал добавления товара в инвентарь

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Shop.Data;
 using Shop.Models;
 using System;
 using System.Collections.Generic;
@@ -12,16 +14,72 @@ namespace Shop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        public List<Rock> Rocks { get; set; }
+        public List<Niddle> Niddles { get; set; }
+        public List<Myinventory> Myinventorys { get; set; }
+        Myinventory my = new Myinventory();
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _context = db;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Add(string a, string b, string c)
+        {
+            my.UserName = a;
+            my.ProductName = b;
+            if (c == "Rock")
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Myinventorys.Add(my);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("RockCatalog");
+                }
+            } else if (c == "Niddle")
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Myinventorys.Add(my);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("NiddleCatalog");
+                }
+
+            }
+            return RedirectToAction("Shoes");
+        }
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult MyInv()
+        {
+            ViewBag.Name = "Мои зачарования";
+            Rocks = _context.Rocks.AsNoTracking().ToList();
+            Niddles = _context.Niddles.AsNoTracking().ToList();
+            Myinventorys = _context.Myinventorys.AsNoTracking().ToList();
+            var model = Tuple.Create<IEnumerable<Rock>, IEnumerable<Niddle>, IEnumerable<Myinventory>>(Rocks, Niddles, Myinventorys);
+            return View(model);
+        }
+
+        public IActionResult RockCatalog()
+        {
+            ViewBag.CatalogName = "Зачарованные камни";
+            Rocks = _context.Rocks.AsNoTracking().ToList();
+            return View(Rocks);
+        }
+
+        public IActionResult NiddleCatalog()
+        {
+            ViewBag.CatalogName = "Зачарованные иголки";
+            Niddles = _context.Niddles.AsNoTracking().ToList();
+            return View(Niddles);
+        }
+
+
 
         public IActionResult Privacy()
         {
