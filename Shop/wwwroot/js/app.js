51491939f860c47ae90bdb372c95ce4f5b674fcd
Владислав Харламов <vladxreal@gmail.com>
2022-10-20 23:55:07 +0300

    Modifyed main page

---
+++ Shop/wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿VANTA.CELLS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color1: 0xc537a,
+    color2: 0x2da8cf,
+    size: 1.60,
+    speed: 0.80
+})
